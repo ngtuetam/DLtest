0cb3fa18b5a0efc0a3e93ed6efe40b0e3c74f75e
Tue Tam Nguyen <82007175+ngtuetam@users.noreply.github.com>
2021-09-23 21:08:50 +0700

    fibo

---
+++ Recursive Algo/fibo.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+long long F[1005];
+
+
+long long fibo(int n) {
+    if (F[n] != -1){
+        return F[n];
+    }
+    if (n==0 || n ==1) {
+        return F[n] = 1 ;
+    }
+    F[n] = fibo(n-1) + fibo(n-2);
+    return F[n];
+}
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 0; i<=n;i++){
+        F[i] = -1;
+    }
+    cout << fibo(n);
+}
